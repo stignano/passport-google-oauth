00d44f1131e18bfb0ccdda59ef3b459bc220fdce
Tom Ierino <tomierino@gmail.com>
2020-08-07 23:37:20 +0800

    routing & middleware

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Welcome from "./Welcome";
 
 const App = () => {
-  return <div>Bruh</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route exact path="/" component={Welcome}></Route>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/Welcome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Welcome() {
+  return <div>Hi :)</div>;
+}
+
+export default Welcome;
